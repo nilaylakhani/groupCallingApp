1e984a4056aca6883e6b98423960469732927d39
nilaylakhani <nilaylakhani15@gmail.com>
2021-04-12 21:01:03 +0530

    Working now video calling

--- lib/screens/meetingScreen.dart
+++ lib/screens/meetingScreen.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'dart:math';
 import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
@@ -7,6 +6,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:group_video_call_app/utilities/constant.dart';
+import 'package:group_video_call_app/utilities/universalData.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class MeetingScreen extends StatefulWidget {
@@ -17,22 +17,20 @@ class MeetingScreen extends StatefulWidget {
 }
 
 class _State extends State<MeetingScreen> {
-  String channelId = CHANNEL1;
+  String channelId = 'testChannelName';
   bool isJoined = false, switchCamera = true, switchRender = true;
   List<int> remoteUid = [];
-  TextEditingController _controller;
 
   @override
   void initState() {
     super.initState();
-    _controller = TextEditingController(text: channelId);
+
     this._initEngine();
   }
 
   @override
   void dispose() {
     super.dispose();
-    this._leaveChannel();
     widget._engine?.destroy();
   }
 
@@ -43,15 +41,16 @@ class _State extends State<MeetingScreen> {
 
     await widget._engine.enableVideo();
     await widget._engine.startPreview();
-
     await widget._engine.setChannelProfile(ChannelProfile.LiveBroadcasting);
     await widget._engine.setClientRole(ClientRole.Broadcaster);
+    _joinChannel();
   }
 
   _addListeners() {
     widget._engine?.setEventHandler(RtcEngineEventHandler(
       joinChannelSuccess: (channel, uid, elapsed) {
         print('joinChannelSuccess ${channel} ${uid} ${elapsed}');
+        print('________________JOin Success.___________________');
         setState(() {
           isJoined = true;
         });
@@ -82,12 +81,13 @@ class _State extends State<MeetingScreen> {
     if (defaultTargetPlatform == TargetPlatform.android) {
       await [Permission.microphone, Permission.camera].request();
     }
-    await widget._engine
-        ?.joinChannel(AGORA_TOKEN, channelId, null, Random().nextInt(100));
+    await widget._engine?.joinChannel(
+        '', channelId, UniversalData.username, Random().nextInt(2000));
   }
 
   _leaveChannel() async {
     await widget._engine?.leaveChannel();
+    Navigator.pop(context);
   }
 
   _switchCamera() {
@@ -109,46 +109,60 @@ class _State extends State<MeetingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Column(
-          children: [
-            _renderVideo(),
-          ],
+    return Scaffold(
+      floatingActionButton: Align(
+        alignment: Alignment.bottomCenter,
+        child: FloatingActionButton(
+          backgroundColor: Colors.red,
+          onPressed: () => {_leaveChannel()},
+          tooltip: 'End',
+          child: Icon(Icons.call_end_sharp, color: Colors.white),
         ),
-        Align(
-          alignment: Alignment.bottomRight,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
+      ),
+      body: Stack(
+        children: [
+          Column(
             children: [
-              MaterialButton(
-                color: Colors.deepPurple,
-                onPressed: this._switchCamera,
-                child: Text('Camera ${switchCamera ? 'switch' : 'switch'}'),
-              ),
-              SizedBox(height: 30),
-              SizedBox(width: MediaQuery.of(context).size.width),
-              MaterialButton(
-                color: isJoined ? Colors.red : Colors.green,
-                onPressed: isJoined ? this._leaveChannel : this._joinChannel,
-                child: Text('${isJoined ? 'End Call' : 'Start Call'}'),
-              ),
-              SizedBox(height: 30),
+              _renderVideo(),
             ],
           ),
-        )
-      ],
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              SizedBox(width: MediaQuery.of(context).size.width),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: SizedBox.fromSize(
+                  size: Size(56, 56),
+                  child: ClipOval(
+                    child: MaterialButton(
+                      color: Colors.blue,
+                      padding: EdgeInsets.all(10.0),
+                      onPressed: () {
+                        this._switchCamera();
+                      },
+                      child:
+                          Icon(Icons.flip_camera_android, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 
   _renderVideo() {
     return Expanded(
+      flex: 1,
       child: Stack(
         children: [
           RtcLocalView.SurfaceView(),
           Padding(
-            padding: const EdgeInsets.only(left: 10.0, top: 50.0),
+            padding: const EdgeInsets.only(left: 10.0, top: 80),
             child: Align(
               alignment: Alignment.topLeft,
               child: SingleChildScrollView(
@@ -157,14 +171,11 @@ class _State extends State<MeetingScreen> {
                   children: List.of(remoteUid.map(
                     (e) => GestureDetector(
                       onTap: this._switchRender,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          width: 120,
-                          height: 120,
-                          child: RtcRemoteView.SurfaceView(
-                            uid: e,
-                          ),
+                      child: Container(
+                        width: 120,
+                        height: 200,
+                        child: RtcRemoteView.SurfaceView(
+                          uid: e,
                         ),
                       ),
                     ),
--- lib/utilities/constant.dart
+++ lib/utilities/constant.dart
@@ -1,8 +1,5 @@
-const AGORA_APP_ID = 'edd90f4701ae4ad5aca8a3da6529e664';
-const AGORA_TOKEN =
-    '006edd90f4701ae4ad5aca8a3da6529e664IABmboZ0X0rXFlk4HFsT4s20ZXj8anewZ0tozio71VvyvBKuJ9wAAAAAEAC74Hh67sNxYAEAAQDtw3Fg';
-
-const CHANNEL1 = 'mySecretChannel';
+const AGORA_APP_ID = 'b8de2d359baf40879e8fc39abbd4cf9e';
+const CHANNEL1 = '';
 // const CHANNEL2 = 'callingAppChannel2';
 // const CHANNEL3 = 'callingAppChannel3';
 // const CHANNEL4 = 'callingAppChannel4';
